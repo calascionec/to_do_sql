66d741667a7753963cdcf2b7df3d4b916f830ea6
filipe silvestre & chris calascione <student@epicodus.com>
2015-08-17 14:28:40 -0700

    finish cowboy functionalty for mysql

--- app/app.php
+++ app/app.php
@@ -2,6 +2,7 @@
   //dependencies
   require_once __DIR__."/../vendor/autoload.php";
   require_once __DIR__."/../src/Task.php";
+  require_once __DIR__."/../src/Category.php";
 
 
 
@@ -12,7 +13,7 @@
       'twig.path' => __DIR__.'/../views'));
 
   //Database
-  $server = 'mysql:host=localhost; db=to_do'; //connect to the localhost and DB 'to_do'
+  $server = 'mysql:host=localhost;dbname=to_do'; //connect to the localhost and DB 'to_do'
   $username = 'root';
   $password = 'root';
   $DB = new PDO($server, $username, $password);
@@ -21,34 +22,41 @@
 
   $app->get("/", function() use ($app) {
 
-      return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getTasks(), 'count' => 0));
+      return $app['twig']->render('index.html.twig');
+  });
+
+  $app->get("/tasks", function() use ($app) {
+      return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+  });
+
+  $app->get("/categories", function() use ($app) {
+      return $app['twig']->render('categories.html.twig', array('categories' =>Category::getAll()));
   });
 
   $app->post("/tasks", function() use ($app) {
-    $task = new Task($_POST['description'], $_POST['dueDate']);
+    $task = new Task($_POST['description']);
     $task->save();
-    return $app['twig']->render('task_created.html.twig', array('newtask' => $task));
+    return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
 
   });
 
-  $app->post('/delete_one', function() use ($app) {
+  $app->post('/tasks_deleted', function() use ($app) {
 
-       $tasks = Task::getTasks();
-       unset($tasks[$_POST['count']]);
-       $tasks = array_values($tasks);
+       Task::deleteAll();
 
-     //return $app['twig']->render('delete_one.html.twig');
+     return $app['twig']->render('index.html.twig');
+ });
 
-    return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getTasks(), 'count' => 0));
-  });
+ $app->post("/categories", function() use ($app) {
+     $category = new Category($_POST['name']);
+     $category->save();
+     return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+ });
 
-  $app->post('/delete_tasks', function() use ($app) {
-
-    Task::deleteTasks();
-
-    return $app['twig']->render('tasks_deleted.html.twig');
-
-  });
+ $app->post("/delete_categories", function() use ($app) {
+     Category::deleteAll();
+     return $app['twig']->render('index.html.twig');
+ });
 
   return $app;
 
